ab7c67322b84c7bccf6843a2ba966c19d78fd0ee
Maciej <mkachniarz1@gmail.com>
2018-06-13 17:09:18 +0200

    Adding context to Startup

--- Models/AnimalContext.cs
+++ Models/AnimalContext.cs
@@ -1,7 +1,8 @@
-namespace APBDSinglePage.Models
-{
-    public class AnimalContext
-    {
-        
+using Microsoft.EntityFrameworkCore;
+
+namespace APBDSinglePage.Models {
+    public class AnimalContext : DbContext {
+        public AnimalContext (DbContextOptions<AnimalContext> options) : base (options) { }
+        public DbSet<APBDSinglePage.Models.AnimalModel> Animal { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using APBDSinglePage.Models;
 
 namespace APBDSinglePage
 {
@@ -22,6 +24,9 @@ namespace APBDSinglePage
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<AnimalContext>(options => 
+                                                options.UseSqlite("Data Source=Animal.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
