50db629aab3f103b798779779bd4a71f29f0ea5e
Maciej <mkachniarz1@gmail.com>
2018-06-13 16:25:59 +0200

    Adding animal model

---
+++ Models/AnimalModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APBDSinglePage.Models
+{
+    public class AnimalModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime DateOfBirth { get; set; }    
+    }
+}
